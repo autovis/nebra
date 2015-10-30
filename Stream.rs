27e58acba46125983a3c4c74dc4de0b931e66770
autovis <autovis@users.noreply.github.com>
2015-10-30 08:11:58 -0400

    Update Stream.rs

--- Stream.rs
+++ Stream.rs
@@ -6,28 +6,36 @@ use std::default::Default;
 fn main() {
 
     // int stream
-    let mut stream1 : Stream<i32> = Stream::new("int_stream", 1i32);
+    let mut stream1 : Stream<isize> = Stream::new("int_stream", 10);
     stream1.next();
     stream1.set(1, 0);
     stream1.next();
     stream1.set(2, 0);
     stream1.next();
     stream1.set(3, 0);
-    stream1.set(4, 2);
     stream1.next();
+    stream1.set(4, 0);
+    //stream1.next();
+    
     println!("{:?}", stream1);
+    println!("{:?} {:?} {:?}", stream1.get(0), stream1.get(1), stream1.get(2));
+    println!("idx: {:?}", stream1.index);
 
     // str stream
-    let mut stream2 : Stream<&'static str> = Stream::new("str_stream", 2i32);
+    let mut stream2 : Stream<&'static str> = Stream::new("str_stream", 3);
     stream2.next();
-    stream2.set("asdf", 0);
+    stream2.set("abc", 0);
+    stream2.next();
+    stream2.set("def", 0);
+    stream2.next();
+    stream2.set("ghi", 0);
     stream2.next();
     println!("{:?}", stream2);
 
     // Option<int> stream
-    let mut stream3 : Stream<Option<i32>> = Stream::new("optint_stream", 3i32);
+    let mut stream3 : Stream<Option<i32>> = Stream::new("optint_stream", 3);
     stream3.next();
-    stream3.set(Some(10i32), 0);
+    stream3.set(Some(10), 0);
     stream3.next();
     println!("{:?}", stream3);
 }
@@ -36,10 +44,8 @@ fn main() {
 
 #[allow(dead_code)]
 pub struct Stream<T> {
-
-    params: i32,
 	id: &'static str,
-
+    size: usize,
 	index: isize,
 	buffer: Vec<T>,
 }
@@ -47,20 +53,16 @@ pub struct Stream<T> {
 #[allow(dead_code)]
 impl<T:Default> Stream<T> {
 
-    pub fn new(id: &'static str, params: i32) -> Stream<T> {
-
+    pub fn new(id: &'static str, size: usize) -> Stream<T> {
         Stream {
-            params: params,
-            buffer: Vec::with_capacity(100),
+            size: size,
+            buffer: Vec::with_capacity(size),
             index: -1,
             id: id
         }
-
     }
 
 	pub fn get(&self, bars_ago: usize) -> &T {
-
-        // sanity checks here
         let size = self.buffer.capacity();
         &self.buffer[((self.index as usize) - bars_ago) % size]
 	}
